29533924b744182146b0b31f76678469ea12ba5e
Amos Asherov <amos@desktop.com>
2024-03-25 17:32:17 +0200

    Fix: build per environment

--- constants.ts
+++ constants.ts
@@ -1,13 +1,24 @@
 export const APP_PREFIX = "threewalls-app";
 export const APP_ICON_PATH = "public/icon.png";
 
-// export const API_URL = "https://api.app.3walls.org/api/v2"; // * PROD
-// export const WEBSITE_URL = "https://app.3walls.org"; // * PROD
+var _API_URL;
+var _WEBSITE_URL;
 
-// export const API_URL = "https://api.dev.3walls.org/api/v2"; // * DEV
-// export const WEBSITE_URL = "https://dev.3walls.org"; // * DEV
+switch (process.env.NODE_ENV) {
+    case 'local':
+        _API_URL = "http://127.0.0.1:7000/api/v2"; // * LOCAL
+        _WEBSITE_URL = "http://localhost:5173"; // * LOCAL
+        break;
+    case 'development':
+        _API_URL = "https://api.dev.3walls.org/api/v2"; // * DEV
+        _WEBSITE_URL = "https://dev.3walls.org"; // * DEV
+        break;
+    default:
+        _API_URL = "https://api.app.3walls.org/api/v2"; // * PROD
+        _WEBSITE_URL = "https://app.3walls.org"; // * PROD
+        break;   
+}
 
-export const API_URL = `${process.env.API_URL || "http://127.0.0.1:7000"}/api/v2`; // * LOCAL
-export const WEBSITE_URL = process.env.WEBSITE_URL || "http://localhost:5173"; // * LOCAL
-
+export const API_URL = _API_URL;
+export const WEBSITE_URL = _WEBSITE_URL
 export const SCREENS_COUNT = 3;
